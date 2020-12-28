fcec0fa2b1ae0ef0d41560f788a91bc28ccc235a
Kareem Fouad <49077828+Kareem-Fouad@users.noreply.github.com>
2020-12-28 02:45:57 +0200

    Update Udacity-Store-data-EDA.py

--- Udacity-Store-data-EDA.py
+++ Udacity-Store-data-EDA.py
@@ -15,7 +15,6 @@ df.head()
 # In[2]:
 
 
-
 df["week"] = pd.to_datetime(df["week"])
 df.info()
 df['Year'] = df['week'].dt.year
@@ -28,37 +27,70 @@ df.head()
 
 
 # explore data
-filt = df["Year"] == df['Year'].max()
-df_lastM = df[(df["Year"] == df["Year"].max()) & (df["Month"] == df[filt]["Month"].max()  )]
-display(df_lastM.head())
+x = df.loc[ : , : 'storeE']
+sns.distplot(x["storeA"]);
 
 
 # In[4]:
 
 
-filt = df["Year"] == df['Year'].max()
-df[filt]
-#for i in df[filt]["Month"]:
-df[filt]["Month"].max()  
+sns.distplot(x["storeB"]);
 
 
 # In[5]:
 
 
+sns.distplot(x["storeC"]);
+
+
+# In[6]:
+
+
+sns.distplot(x["storeD"]);
+
+
+# In[7]:
+
+
+sns.distplot(x["storeE"]);
+
+
+# In[8]:
+
+
+x.hist(figsize=(8, 8));
+plt.show()
+
+
+# In[9]:
+
+
 # sales for the last month
+filt = df["Year"] == df['Year'].max()
+df[filt]
+df[filt]["Month"].max()  
+
+
+# In[10]:
+
+
+# sales for the last month
+filt = df["Year"] == df['Year'].max()
+df_lastM = df[(df["Year"] == df["Year"].max()) & (df["Month"] == df[filt]["Month"].max()  )]
+display(df_lastM.head())
 display(df_lastM.head())
 display(df_lastM.loc[ : , :"storeE"].describe())
 display(df_lastM.loc[ : , :"storeE"].sum())
 
 
-# In[6]:
+# In[11]:
 
 
 # average sales
 df.loc[ : , :"storeE"].mean()
 
 
-# In[7]:
+# In[12]:
 
 
 # sales for the week of March 13th, 2016
@@ -72,7 +104,7 @@ display(df_lastW12.loc[ : , :"storeE"].describe())
 display(df_lastW12.loc[ : , :"storeE"].sum())
 
 
-# In[8]:
+# In[13]:
 
 
 # sales for the latest 3-month periods
@@ -81,44 +113,6 @@ display(df_last3M.head())
 display(df_last3M.loc[ : , :"storeE"].describe())
 display(df_last3M.loc[ : , :"storeE"].sum())
 display(df_last3M.loc[ : , :"storeE"].mean())
-
-
-# In[19]:
-
-
-x = df.loc[ : , : 'storeE']
-sns.distplot(x["storeA"]);
-
-
-# In[20]:
-
-
-sns.distplot(x["storeB"]);
-
-
-# In[21]:
-
-
-sns.distplot(x["storeC"]);
-
-
-# In[22]:
-
-
-sns.distplot(x["storeD"]);
-
-
-# In[23]:
-
-
-sns.distplot(x["storeE"]);
-
-
-# In[24]:
-
-
-x.hist(figsize=(8, 8));
-plt.show()
 
 
 # In[ ]:
